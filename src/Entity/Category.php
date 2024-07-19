9e36f04013709e5784ed0d78d9904fe5bd018349
Briano <briano.manitra@gmail.com>
2024-07-19 08:05:01 +0200

    parking space api

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: CategoryRepository::class)]
@@ -21,6 +23,17 @@ class Category
 
     #[ORM\Column]
     private ?float $length = null;
+
+    /**
+     * @var Collection<int, ParkingSpace>
+     */
+    #[ORM\OneToMany(targetEntity: ParkingSpace::class, mappedBy: 'Categorie')]
+    private Collection $parkingSpaces;
+
+    public function __construct()
+    {
+        $this->parkingSpaces = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -59,6 +72,36 @@ class Category
     public function setLength(float $length): static
     {
         $this->length = $length;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, ParkingSpace>
+     */
+    public function getParkingSpaces(): Collection
+    {
+        return $this->parkingSpaces;
+    }
+
+    public function addParkingSpace(ParkingSpace $parkingSpace): static
+    {
+        if (!$this->parkingSpaces->contains($parkingSpace)) {
+            $this->parkingSpaces->add($parkingSpace);
+            $parkingSpace->setCategorie($this);
+        }
+
+        return $this;
+    }
+
+    public function removeParkingSpace(ParkingSpace $parkingSpace): static
+    {
+        if ($this->parkingSpaces->removeElement($parkingSpace)) {
+            // set the owning side to null (unless already changed)
+            if ($parkingSpace->getCategorie() === $this) {
+                $parkingSpace->setCategorie(null);
+            }
+        }
 
         return $this;
     }
